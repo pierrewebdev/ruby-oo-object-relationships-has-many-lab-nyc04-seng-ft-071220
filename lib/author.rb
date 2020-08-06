53acbe7e35245f7f1d9e88c0e059a70a5742d0a8
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-06 12:51:35 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,7 +7,7 @@ class Author
   end
   
   def posts
-    @posts << Post.all.select {|post|post.author == self}
+    @posts << Post.all.select {|post|post.author == self}.flatten
     binding.pry
   end
   
