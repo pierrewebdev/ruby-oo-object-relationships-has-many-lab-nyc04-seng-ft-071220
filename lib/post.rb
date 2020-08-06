a912ef44bf85671627c83d53e8ef451e6b75c981
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-06 12:45:12 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -3,7 +3,9 @@ class Post
   #class variable
   @@all = []
   
-  def initialize(title)
-    @title = t
+  def initialize(title,author = nil)
+    @title = title
+    
+    @@all << self #pushes new instances of this class into the class variable
   end
 end
