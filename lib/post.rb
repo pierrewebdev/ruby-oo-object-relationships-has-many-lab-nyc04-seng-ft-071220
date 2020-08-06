87c249acbeb0a7bfee39a6c5ba46445f9cef1621
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-06 12:47:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,22 @@
 class Post
+  attr_accesor :author
+  
   
   #class variable
   @@all = []
   
   def initialize(title,author = nil)
     @title = title
-    
+    @author = author
     @@all << self #pushes new instances of this class into the class variable
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def author_name
+    if self.author
+      self.author.name
   end
 end
