566d86df4a05ec5aa55e1553f3dd1216b38600be
Patrick Pierre <patrick.pierre000@gmail.com>
2020-08-06 13:16:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,8 +10,13 @@ class Author
     self.posts = Post.all.select{|single_post|single_post.author == self}
   end
   
-  def add_post(new_post)
-    new_post.author = self
+  def add_post(post_to_add)
+    post_to_add.author = self
+  end
+  
+  def add_post_by_title(new_post)
+    new_post = Post.new(new_post)
+    new_past.author = self
   end
   
   
